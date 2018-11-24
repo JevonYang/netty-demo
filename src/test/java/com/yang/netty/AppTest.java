743395453144b6bd919754a004d8182670996b56
JevonYang <yangzijing08@gmail.com>
2018-11-24 17:47:30 +0800

    deserialization

--- src/test/java/com/yang/netty/AppTest.java
+++ src/test/java/com/yang/netty/AppTest.java
@@ -2,6 +2,7 @@ package com.yang.netty;
 
 import static org.junit.Assert.assertTrue;
 
+import com.google.protobuf.ByteString;
 import org.junit.Test;
 
 /**
@@ -13,21 +14,26 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswerWithTrue() throws Exception
     {
         assertTrue( true );
         PersonModel.Person.Builder builder = PersonModel.Person.newBuilder();
         builder.setId(1);
         builder.setName("yang");
-        builder.setName("yangzijing08@gmail.com");
+        builder.setEmail("yangzijing08@gmail.com");
 
         PersonModel.Person person = builder.build();
         System.out.println("Before:" + person);
 
-        System.out.println("Bytes: ");
-        for (byte b : person.toByteArray()) {
-            System.out.println(b);
-        }
+        ByteString data = person.toByteString();
+
+        PersonModel.Person fromBytes = PersonModel.Person.parseFrom(data);
+        System.out.println("id :" + fromBytes.getId());
+        System.out.println("name ：" + fromBytes.getName());
+        System.out.println("email: " + fromBytes.getEmail());
+
+        System.out.println("after: " + data.toString("UTF-8"));
+
 
     }
 }
